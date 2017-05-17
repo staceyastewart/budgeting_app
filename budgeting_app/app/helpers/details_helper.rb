e052f40ec6a3cf3358003de7090c7e0937812011
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-05-17 18:53:43 -0400

    renders totals on details page

--- budgeting_app/app/helpers/details_helper.rb
+++ budgeting_app/app/helpers/details_helper.rb
@@ -20,4 +20,23 @@ module DetailsHelper
     end
   end
 
+  def total_display(category)
+    total_budget = category.month_budget_by_category(@month, @year, current_user.id)
+    total_expense = category.month_expense_by_category(@month, @year, current_user.id)
+    if total_budget && total_expense
+      difference = total_budget - total_expense
+      if difference < 0
+        return "<td> #{number_to_currency(total_budget)} </td> <td> #{number_to_currency(total_expense)} </td> <td style='color:red'> #{number_to_currency(total_budget - total_expense)}</td>".html_safe
+      else
+        return "<td> #{number_to_currency(total_budget)} </td> <td> #{number_to_currency(total_expense)} </td> <td> #{number_to_currency(total_budget - total_expense)}</td>".html_safe
+      end
+    elsif total_budget
+      return "<td> #{number_to_currency(total_budget)} </td> <td> 0</td> <td> #{number_to_currency(total_budget)}</td>".html_safe
+    elsif total_expense
+      return "<td> 0 </td> <td> #{number_to_currency(total_expense)} </td> <td style='color:red'> #{number_to_currency(-total_expense)}</td>".html_safe
+    else
+      return "<td> 0 </td> <td> 0 </td> <td> 0 </td>".html_safe
+    end
+  end
+
 end
