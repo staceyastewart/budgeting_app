f248b816792760509225e06c3ad32f9b1fe64a65
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-05-17 18:43:03 -0400

    creates methods to total monthly expenses and budgets

--- budgeting_app/app/helpers/details_helper.rb
+++ budgeting_app/app/helpers/details_helper.rb
@@ -1,2 +1,23 @@
 module DetailsHelper
+
+  def budget_display(subcategory)
+    total_budget = subcategory.total_budget(@month, @year, current_user.id)
+    total_expense = subcategory.total_expense(@month, @year, current_user.id)
+
+    if total_budget && total_expense
+      difference = total_budget - total_expense
+      if difference < 0
+        return "<td> #{number_to_currency(total_budget)} </td> <td> #{number_to_currency(total_expense)} </td> <td style='color:red'> #{number_to_currency(total_budget - total_expense)}</td>".html_safe
+      else
+        return "<td> #{number_to_currency(total_budget)} </td> <td> #{number_to_currency(total_expense)} </td> <td> #{number_to_currency(total_budget - total_expense)}</td>".html_safe
+      end
+    elsif total_budget
+      return "<td> #{number_to_currency(total_budget)} </td> <td> 0</td> <td> #{number_to_currency(total_budget)}</td>".html_safe
+    elsif total_expense
+      return "<td> 0 </td> <td> #{number_to_currency(total_expense)} </td> <td style='color:red'> #{number_to_currency(-total_expense)}</td>".html_safe
+    else
+      return "<td> 0 </td> <td> 0 </td> <td> 0 </td>".html_safe
+    end
+  end
+
 end
--- budgeting_app/app/models/user.rb
+++ budgeting_app/app/models/user.rb
@@ -7,4 +7,24 @@ class User < ApplicationRecord
   has_many :incomes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def total_month_budget(month, year)
+    user = self.id
+    month_budget_arr = []
+    categories.each do |category|
+      month_budget_arr.push(category.month_budget_by_category(month, year, user))
+    end
+    return month_budget_arr.reduce(:+)
+  end
+
+  def total_month_expense(month, year)
+    user = self.id
+    month_exp_arr = []
+    categories.each do |category|
+      month_exp_arr.push(category.month_expense_by_category(month, year, user))
+    end
+    return month_exp_arr.reduce(:+)
+  end
+
 end
