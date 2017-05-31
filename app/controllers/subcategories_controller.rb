5299602e931c7b8ffcf3c07da5bf7efce6eec40d
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-05-31 14:56:31 -0400

    adds update and delete for subcategories

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -3,7 +3,7 @@ class SubcategoriesController < ApplicationController
   def index
     @user = current_user.id
     @cats = Category.where(:user_id => current_user.id)
-    @subcats = Subcategory.where(:user_id => current_user.id).pluck(:name, :category_id)
+    @subcats = Subcategory.where(:user_id => current_user.id).pluck(:name, :category_id, :id)
   end
 
   def create
@@ -15,6 +15,21 @@ class SubcategoriesController < ApplicationController
       category_id: params[:subcategories][:category],
       user_id: params[:user_id])
     redirect_to :back
+  end
+
+  def update
+    @subcategoryToEdit = Subcategory.find_by_id(params[:id])
+    return unless @subcategoryToEdit.user_id === current_user.id
+      @subcategoryToEdit.update(
+        name: params[:name])
+      redirect_to :back
+  end
+
+  def destroy
+    toDelete = Subcategory.find_by_id(params[:id])
+    return unless toDelete.user_id === current_user.id
+      toDelete.destroy
+      redirect_to :back
   end
 
 end
