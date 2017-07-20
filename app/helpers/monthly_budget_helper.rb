ac34dce3fca44224a4525500434253475d1ad156
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-07-20 15:20:13 -0400

    creates catch on monthly budget setup

--- app/helpers/monthly_budget_helper.rb
+++ app/helpers/monthly_budget_helper.rb
@@ -3,8 +3,15 @@ module MonthlyBudgetHelper
   def each_category_has_child(user)
     user.categories.each do |category|
       if category.subcategories.empty?
-        return false        
+        return false
       end
+    end
+    return true
+  end
+
+  def user_has_categories(user)
+    if user.categories.empty?
+        return false
     end
     return true
   end
