5bfea2982fc44f4945ad84bb9fedf80ee97340a7
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-07-28 16:57:39 -0400

    refactors category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,31 +1,43 @@
 class CategoriesController < ApplicationController
   def index
     @user = current_user.id
-    @categories = Category.where(:user_id => current_user.id)
+    @categories = find_all_categories
 
   end
 
   def create
     return unless current_user.id.to_s == params[:user_id]
-    Category.create(
-      title: params[:title],
-      user_id: params[:user_id])
+    Category.create(category_params)
     redirect_to :back
   end
 
   def update
-    @categoryToEdit = Category.find_by_id(params[:id])
+    @categoryToEdit = find_category_to_edit
     return unless @categoryToEdit.user_id === current_user.id
-      @categoryToEdit.update(
-        title: params[:title])
+      @categoryToEdit.update(category_params)
       redirect_to :back
   end
 
   def destroy
-    toDelete = Category.find_by_id(params[:id])
+    toDelete = find_category_to_edit
     return unless toDelete.user_id === current_user.id
       toDelete.destroy
       redirect_to :back
   end
+
+  private
+
+  def category_params
+    params.permit(:title, :user_id)
+  end
+
+  def find_all_categories
+    Category.where(:user_id => current_user.id)
+  end
+
+  def find_category_to_edit
+    Category.find_by_id(params[:id])
+  end
+
 
 end
