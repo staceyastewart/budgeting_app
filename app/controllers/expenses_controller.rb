c79531574ccf93494ade3e668254dd18a87e5617
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-08-01 14:47:20 -0400

    refactors expense update

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -20,8 +20,16 @@ class ExpensesController < ApplicationController
     months_array = ["ALL", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     if (params["month"] == "ALL")
       months_array.each do |month|
-        new_month = month
-        Expense.create(description: params[:description], amount: params[:amount], day: params[:expenses][:day], month: new_month, month_num: months_array.index(new_month), year: params[:year], user_id: params[:user_id], isrecurring: true, subcategory_id: params[:expenses][:subcategory])
+        Expense.create(
+          description: params[:description],
+          amount: params[:amount],
+          day: params[:expenses][:day],
+          month: month,
+          month_num: months_array.index(month),
+          year: params[:year],
+          user_id: params[:user_id],
+          isrecurring: true,
+          subcategory_id: params[:expenses][:subcategory])
       end
       redirect_to :back
     else
@@ -33,34 +41,42 @@ class ExpensesController < ApplicationController
         month_num: months_array.index(params[:month]),
         year: params[:year],
         user_id: params[:user_id],
-        subcategory_id: params[:expense][:subcategory] )
+        subcategory_id: params[:expense][:subcategory])
       redirect_to :back
     end
   end
 
+  # def update
+  #   expenseToEdit = expense_to_edit
+  #   return unless expenseToEdit.user_id === current_user.id
+  #   if expenseToEdit.month == "ALL"
+  #     allToUpdate = expenses_to_update(expenseToEdit)
+  #     allToUpdate.update(expense_params)
+  #     redirect_to :back
+  #   else
+  #     puts params
+  #     if params[:expense][:subcategory].blank?
+  #       expenseToEdit.update(expense_params)
+  #       redirect_to :back
+  #     else
+  #       expenseToEdit.update(
+  #         description: params[:description],
+  #         amount: params[:amount],
+  #         subcategory: Subcategory.find_by_id(params[:expense][:subcategory]),
+  #         day: params[:day])
+  #       # need to figure out how to refactor to update subcategory using strong params, below does not work
+  #       # expenseToEdit.update(expense_params)
+  #       redirect_to :back
+  #     end
+  #   end
+  # end
+
   def update
+    # this update method is not updating the subcategory if it is changed
     expenseToEdit = expense_to_edit
     return unless expenseToEdit.user_id === current_user.id
-    if expenseToEdit.month == "ALL"
-      allToUpdate = expenses_to_update(expenseToEdit)
-      allToUpdate.update(expense_params)
+    expense_to_edit.update_expense(expense_params, expenseToEdit, current_user.id)
       redirect_to :back
-    else
-      puts params
-      if params[:expense][:subcategory].blank?
-        expenseToEdit.update(expense_params)
-        redirect_to :back
-      else
-        expenseToEdit.update(
-          description: params[:description],
-          amount: params[:amount],
-          subcategory: Subcategory.find_by_id(params[:expense][:subcategory]),
-          day: params[:day])
-        # need to figure out how to refactor to update subcategory using strong params, below does not work
-        # expenseToEdit.update(expense_params)
-        redirect_to :back
-      end
-    end
   end
 
   def destroy
@@ -99,7 +115,7 @@ class ExpensesController < ApplicationController
   end
 
   def expense_params
-    params.permit(:description, :amount, :day, :subcategory)
+    params.permit(:description, :amount, :day, :expense, :subcategory_id)
   end
 
   def find_expense(month, year, user)
@@ -114,9 +130,9 @@ class ExpensesController < ApplicationController
     Subcategory.find_by_id(params[:expense][:subcategory])
   end
 
-  def expenses_to_update(expense)
-    Expense.where(day: expense[:day]).where(description: expense[:description]).where(year: expense[:year]).where(:user_id => current_user.id)
-  end
+  # def expenses_to_update(expense)
+  #   Expense.where(day: expense[:day]).where(description: expense[:description]).where(year: expense[:year]).where(:user_id => current_user.id)
+  # end
 
   def delete_all_month(record_to_delete)
     Expense.where(day: record_to_delete[:day]).where(description: record_to_delete[:description]).where(year: record_to_delete[:year]).where(:user_id => current_user.id)
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,5 +2,19 @@ class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :subcategory
 
+  def update_expense(expense_params, expenseToEdit, user)
+    if expenseToEdit.month == "ALL"
+      allToUpdate = expenses_to_update(expenseToEdit, user)
+      allToUpdate.update(expense_params)
+    else
+      expenseToEdit.update(expense_params)
+    end
+  end
+
+  def expenses_to_update(expense, user)
+    Expense.where(day: expense[:day]).where(description: expense[:description]).where(year: expense[:year]).where(:user_id => user)
+  end
+
+
 
 end
