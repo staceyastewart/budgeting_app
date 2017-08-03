d1192e8ea4a5480efda23930aadc5343613b7872
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-08-03 17:14:34 -0400

    implements private methods in details controller

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -3,8 +3,19 @@ class DetailsController < ApplicationController
   def index
     @month = params[:month]
     @year = params[:year]
-    @categories = Category.where(user_id: current_user.id)
-    @user = User.find_by_id(current_user.id)
+    @categories = find_categories
+    @user = set_user
+  end
+
+  private
+
+  def set_user
+    User.find_by_id(current_user.id)
+  end
+
+
+  def find_categories
+    Category.where(user_id: current_user.id)
   end
 
 end
