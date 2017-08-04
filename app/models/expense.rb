f40dfc32b170c0d268ddc8ee7bd19568a75e37cc
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-08-04 15:51:49 -0400

    removes unused expense methods

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,21 +2,4 @@ class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :subcategory
 
-  def update_expense(expense_params, update_params, expenseToEdit, user)
-    if expenseToEdit.month == "ALL"
-      allToUpdate = expenses_to_update(expenseToEdit, user)
-      allToUpdate.update(expense_params)
-      allToUpdate.update(update_params)
-    else
-      expenseToEdit.update(expense_params)
-      expenseToEdit.update(update_params)
-    end
-  end
-
-  def expenses_to_update(expense, user)
-    Expense.where(day: expense[:day]).where(description: expense[:description]).where(year: expense[:year]).where(:user_id => user)
-  end
-
-
-
 end
